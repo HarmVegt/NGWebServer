7bacdba4ffc3c91d68160fa81dcb8a5b8ee48ef8
Harm van der Vegt <harmvegt@gmail.com>
2019-05-09 14:18:50 +0200

    Initial deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,20 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "ngmanager"
+set :repo_url, "https://github.com/HarmVanDerVegt/NGWebServer"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+set :keep_releases, 5
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '188.166.54.36', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
