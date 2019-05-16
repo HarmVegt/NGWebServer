252e2e262d17600d62f84f0db22edeae9317cedd
Harm van der Vegt <harmvegt@gmail.com>
2019-05-16 14:41:59 +0200

    Better random generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-MAX_CUSTOMERS = 1000
+MAX_CUSTOMERS = 100
 MAX_DELIVERERS = 30
 
 letters = "abcdefghijklmnopqrstuvwxyz"
@@ -17,28 +17,30 @@ p "Creating Customers"
 MAX_CUSTOMERS.times do
   r = Random.new
 
-  firstname = ""
-  lastname = ""
-  preposition = ""
-  email = ""
+  prepositions = ["van", "van der", "van den", "van de", "de", "", ""]
 
-  r.rand(2..15).times do
-    firstname = firstname + letters[r.rand(26)]
-  end
+  firstname = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  preposition = prepositions[r.rand(7)]
+  email = Faker::Internet.unique.email
 
-  r.rand(3..16).times do
-    lastname = lastname + letters[r.rand(26)]
-  end
-
-  r.rand(2..4).times do
-    preposition = preposition + letters[r.rand(26)]
-  end
-
-  r.rand(5).times do
-    email = email + letters[r.rand(26)]
-  end
-
-  email = email + "@random.org"
+  # "#{r.rand(2..15).times do
+  #   firstname = firstname + letters[r.rand(26)]
+  # end
+  #
+  # r.rand(3..16).times do
+  #   lastname = lastname + letters[r.rand(26)]
+  # end
+  #
+  # r.rand(2..4).times do
+  #   preposition = preposition + letters[r.rand(26)]
+  # end
+  #
+  # r.rand(5).times do
+  #   email = email + letters[r.rand(26)]
+  # end
+  #
+  # email = email + "@random.org"}"
 
   Customer.create(firstname: firstname, lastname: lastname, preposition: preposition, email: email)
 
@@ -53,8 +55,8 @@ Customer.all.each do |customer|
 
   zipcode = ""
   housenumber = 0
-  street = ""
-  city = ""
+  street = Faker::Address.street_name
+  city = Faker::Address.city
   annex = ""
   x = 0
   y = 0
@@ -67,17 +69,9 @@ Customer.all.each do |customer|
     zipcode = zipcode + letters[r.rand(26)]
   end
 
-  r.rand(5..15).times do
-    street = street + letters[r.rand(26)]
-  end
-
-  r.rand(4..9).times do
-    city = city + letters[r.rand(26)]
-  end
-
   housenumber = r.rand(255)
 
-  r.rand(2).times do
+  r.rand(1).times do
     annex = annex + letters[r.rand(26)]
   end
 
@@ -95,18 +89,10 @@ p "Creating Products"
 300.times do
   r = Random
 
-  name = ""
-  description = ""
+  name = Faker::Appliance.equipment
+  description = Faker::Music.album
   amount = 0
   price = 0.00
-
-  r.rand(6..20).times do
-    name = name + letters[r.rand(26)]
-  end
-
-  r.rand(10..200).times do
-    description = description + letters[r.rand(26)]
-  end
 
   amount = r.rand(1..100)
   price = r.rand(1.00..400.00).round(2)
