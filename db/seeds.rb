26b964948f96c89bfc329ccd524c38f068148741
Harm van der Vegt <harmvegt@gmail.com>
2019-05-22 14:47:36 +0200

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-MAX_CUSTOMERS = 100
+MAX_CUSTOMERS = 500
 MAX_DELIVERERS = 30
 
 letters = "abcdefghijklmnopqrstuvwxyz"
